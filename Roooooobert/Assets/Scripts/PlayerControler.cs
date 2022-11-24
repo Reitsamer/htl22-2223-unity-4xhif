de8e16c7d6ab91d4012376e471870e4f5fb43d8d
RobertResch <190006@studierende.htl-donaustadt.at>
2022-11-24 17:12:13 +0100

    Flo war wieder mal cool und hat alles gemacht

--- Roooooobert/Assets/Scripts/PlayerControler.cs
+++ Roooooobert/Assets/Scripts/PlayerControler.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D))]
 public class PlayerControler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rigBody;
+    private Animator animator;
+
+    [SerializeField, Range(100,1000)]
+    private int speed = 400;
+    [SerializeField, Range(100, 1000)]
+    private int jumpHight = 400;
+
+    private float x;
+    private float jump;
+
+    private bool hasJumped = false;
+
+    [SerializeField] 
+    private LayerMask floorMask;
+
     void Start()
     {
+        rigBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    
+    void FixedUpdate()
+    {
+        //Movement
+        //Left/Right
+        x = Input.GetAxis("Horizontal");
+
+        rigBody.velocity = Vector2.right * x * speed * Time.fixedDeltaTime;
+
+        animator.SetInteger("Speed", Mathf.Abs(x) < 0.5 ? 0 : 1);
+
+        //Jump
+        jump = Input.GetAxis("Jump");
+
+        if (!hasJumped)
+        {
+            rigBody.AddForce(Vector2.up * jump * jumpHight);
+            hasJumped = true;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        
+        //TODO Check if on ground => reset hasJumped
+        //if(Physics.CheckSphere(gameObject.transform.position))
     }
 }
