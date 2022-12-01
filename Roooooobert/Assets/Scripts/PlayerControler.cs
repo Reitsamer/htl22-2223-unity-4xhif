4d5f3db6afb2fa0fa244e27b0d6ce597863ce79f
Blaues_Gemüse <190006@studierende.htl-donaustadt.at>
2022-12-01 17:04:35 +0100

    1.12.2022

--- Roooooobert/Assets/Scripts/PlayerControler.cs
+++ Roooooobert/Assets/Scripts/PlayerControler.cs
@@ -11,6 +11,12 @@ public class PlayerControler : MonoBehaviour
     private Rigidbody2D rigBody;
     private Animator animator;
 
+    [SerializeField] 
+    public Transform groundCheck;
+
+    [SerializeField, Range(0, 2)] 
+    private float groundCheckRadius;
+
     [SerializeField, Range(100,1000)]
     private int speed = 400;
     [SerializeField, Range(100, 1000)]
@@ -36,24 +42,47 @@ public class PlayerControler : MonoBehaviour
         //Left/Right
         x = Input.GetAxis("Horizontal");
 
+        Vector2 currentScale = transform.localScale;
+
+        if (x < 0)
+        {
+            currentScale.x = -1;
+        }
+        else if (x > 0)
+        {
+            currentScale.x = 1;
+        }
+
+        transform.localScale = currentScale;
+
         rigBody.velocity = Vector2.right * x * speed * Time.fixedDeltaTime;
 
         animator.SetInteger("Speed", Mathf.Abs(x) < 0.5 ? 0 : 1);
 
         //Jump
         jump = Input.GetAxis("Jump");
+        
 
-        if (!hasJumped)
+        if (IsGrounded)
         {
             rigBody.AddForce(Vector2.up * jump * jumpHight);
-            hasJumped = true;
         }
-        
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         //TODO Check if on ground => reset hasJumped
-        //if(Physics.CheckSphere(gameObject.transform.position))
+        
     }
+
+    void OnDrawGizmos()
+    {
+        Color color = Gizmos.color;
+        Gizmos.color = IsGrounded ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(groundCheck.transform.position, groundCheckRadius);
+        Gizmos.color = color;
+    }
+
+    private bool IsGrounded
+        => Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, floorMask);
 }
